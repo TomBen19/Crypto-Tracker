1771d480e619746d3115bb4b87fdc9ef734bfc38
“TomBen19“ <t.b1@gmx.net>
2022-08-23 11:28:11 +0200

    Add modal and quantity requirements

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-import {Component, useState} from 'react'
-import {BrowserRouter, Route, Routes, Redirect} from 'react-router-dom'
+import { Component, useState } from "react";
+import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
 
-import logo from './logo.svg';
-import './App.css';
-import AddTransaction from './components/AddTransaction';
-import CoinList from './components/CoinList';
-import Button from 'react-bootstrap/Button';
-import Header from './components/Header'
-import Container from 'react-bootstrap/Container'
-import Transaction from './pages/Transaction'
-import List from './pages/List'
+import logo from "./logo.svg";
+import "./App.css";
+import AddTransaction from "./components/Portfolio";
+import CoinList from "./components/CoinList";
+import Button from "react-bootstrap/Button";
+import Header from "./components/Header";
+import Container from "react-bootstrap/Container";
+import Transaction from "./pages/Transaction";
+import List from "./pages/List";
 
 function App() {
-  
-
-  
   return (
     <div>
       <BrowserRouter>
-
-      <Routes>
-        
-        <Route path='/' exact element={<Transaction/>} />
-        <Route path='exchanges/*' element={<List />}/>
-          
-       
-      </Routes>
+        <Routes>
+          <Route path="/" exact element={<Transaction />} />
+          <Route path="exchanges/*" element={<List />} />
+        </Routes>
       </BrowserRouter>
-      
-      
-      
-      
-      
     </div>
   );
 }
--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -1,54 +1,53 @@
-import {useState} from 'react'
-import AddTransaction from '../components/AddTransaction';
-import CoinList from '../components/CoinList';
-import Button from 'react-bootstrap/Button';
-import Header from '../components/Header'
-import Container from 'react-bootstrap/Container'
+import { useState } from "react";
+import Portfolio from "../components/Portfolio";
+import CoinList from "../components/CoinList";
+import Button from "react-bootstrap/Button";
+import Header from "../components/Header";
+import Container from "react-bootstrap/Container";
 
 const Transaction = () => {
-    const [coinsList, setCoinsList] = useState([]);
+  const [coinsList, setCoinsList] = useState([]);
   const [price, setPrice] = useState();
   const [value, setValue] = useState(0);
 
   async function saveTransaction(coin) {
-    const respone = await fetch('https://crypto-tracker-c3d8d-default-rtdb.europe-west1.firebasedatabase.app/coins.json', {
-    method: 'POST',
-    body: JSON.stringify(coin)
-  
-  });
-}
+    const respone = await fetch(
+      "https://crypto-tracker-c3d8d-default-rtdb.europe-west1.firebasedatabase.app/coins.json",
+      {
+        method: "POST",
+        body: JSON.stringify(coin),
+      }
+    );
+  }
 
-  const addCoinHandler = (uName,uQuantity,  uPreis) => {
-   
-    const roundPortValue = (Number(value) + uQuantity* uPreis).toFixed(2);
+  const addCoinHandler = (uName, uQuantity, uPreis) => {
+    const roundPortValue = (Number(value) + uQuantity * uPreis).toFixed(2);
     setValue(roundPortValue);
     setCoinsList((prevCoinList) => {
       return [
         ...prevCoinList,
-         {name: uName, quantity: uQuantity, preis: uPreis, id: Math.random().toString()},
-      ]
-    })
-    
-    saveTransaction(coinsList[coinsList.length -1])
-    console.log(coinsList[coinsList.length -1])
-    }
-  
-    return (
-        <div>
-        <Header></Header>
-        <Container>
-        <AddTransaction onNewCoin= {addCoinHandler} />
-        <h2>Portfolio Value: {value} $</h2>
-        <CoinList coins ={coinsList}/>
-        </Container>
-        
-        
-        
-        
-        
-        
-      </div>
-    )
+        {
+          name: uName,
+          quantity: uQuantity,
+          preis: uPreis,
+          id: Math.random().toString(),
+        },
+      ];
+    });
 
-}
+    saveTransaction(coinsList[coinsList.length - 1]);
+    console.log(coinsList[coinsList.length - 1]);
+  };
+
+  return (
+    <div>
+      <Header></Header>
+      <Container>
+        <Portfolio onNewCoin={addCoinHandler} />
+        <h2>Portfolio Value: {value} $</h2>
+        <CoinList coins={coinsList} />
+      </Container>
+    </div>
+  );
+};
 export default Transaction;
